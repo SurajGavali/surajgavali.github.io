f827a39a8c047eba7f2feccf2570a0b8eb764681
Suraj Gavali <surajgavali@Surajs-MacBook-Pro.local>
2025-09-18 22:34:56 +0530

    Migrate to Vite and update homepage setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // will be updated for GitHub Pages
+  base: '/surajgavali.github.io/', // will be updated for GitHub Pages
 });
